f1f3d6fb9bd3093d11b918968fa73648e0d275f3
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-11-02 15:22:45 +0200

    update

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -22,22 +22,22 @@ export const Register = () => {
     // }
 
     return (
-        <div className="container"> {/* Use appropriate classnames from your CSS */}
+        <div className={styles.container}> {/* Use appropriate classnames from your CSS */}
             <h2>Register</h2>
             <form /*onSubmit={handleSubmit}*/>
-                <div className="form-group">
+                <div className={styles['form-group']}>
                     <label htmlFor="name">Name:</label>
                     <input type="text" id="name" name="name" /*value={formData.name} onChange={handleChange} required*/ />
                 </div>
-                <div className="form-group">
+                <div className={styles['form-group']}>
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" name="email" /*value={formData.email} onChange={handleChange} required *//>
                 </div>
-                <div className="form-group">
+                <div className={styles['form-group']}>
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" name="password" /*value={formData.password} onChange={handleChange} required*//>
                 </div>
-                <div className="form-group">
+                <div className={styles['form-group']}>
                     <label htmlFor="confirmPassword">Confirm Password:</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" /*value={formData.confirmPassword} onChange={handleChange} required*/ />
                 </div>
